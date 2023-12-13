0c97d361cf5b9636b4f0f9402b132df1b310b81a
EsraaGD <isra.hatim2@gmail.com>
2023-12-13 12:53:57 -0800

    Shell done

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,13 +8,14 @@
 
 size_t find_gap(size_t size)
 {
-    size_t g;
-    g = 1;
+	size_t g;
 
-    while (g <= size / 3)
-        g = g * 3 + 1;
+	g = 1;
 
-    return g;
+	while (g <= size / 3)
+		g = g * 3 + 1;
+
+	return (g);
 }
 
 /**
@@ -25,29 +26,29 @@ size_t find_gap(size_t size)
 
 void shell_sort(int *array, size_t size)
 {
-    int tmp;
-    size_t gap, a, b;
+	int tmp;
+	size_t gap, a, b;
 
-    size_t gap = find_gap(size);
+	gap = find_gap(size);
 
-    if (!array || !size)
-        return;
+	if (!array || !size)
+		return;
 
-    while (gap > 0)
-    {
-        for (size_t a = 0; a < size - gap; a++)
-        {
-            size_t b = a + gap;
-            while (b >= gap && array[b - gap] > array[b])
-            {
-                int tmp = array[b - gap];
-                array[b - gap] = array[b];
-                array[b] = tmp;
-                b -= gap;
-            }
-        }
-        print_array(array, size);
+	while (gap > 0)
+	{
+		for (a = 0; a < size - gap; a++)
+		{
+			b = a + gap;
+			while (b >= gap && array[b - gap] > array[b])
+			{
+				tmp = array[b - gap];
+				array[b - gap] = array[b];
+				array[b] = tmp;
+				b -= gap;
+			}
+		}
+		print_array(array, size);
 
-        gap = (gap - 1) / 3;
-    }
+		gap = (gap - 1) / 3;
+	}
 }
