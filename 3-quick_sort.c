3e5392ee5729b8547541cd90e4ddfe9e13bfb0a2
oovaa <omarvx211@gmail.com>
2023-12-13 16:29:09 +0200

    minor changes at 13-16

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,15 @@
 #include "sort.h"
 
+
+/**
+ * partition - Lomuto partition scheme
+ * @array: Array to be sorted
+ * @start: Starting index of the array or subarray
+ * @end: Ending index of the array or subarray
+ *
+ * Return: Index of the pivot element after partitioning
+ */
+
 int partition(int *array, size_t start, size_t end)
 {
 	int pivot = array[end];
@@ -12,6 +22,14 @@ int partition(int *array, size_t start, size_t end)
 	swap(&array[++i], &array[end]);
 	return (i);
 }
+
+
+/**
+ * quick_rec - Recursively sorts a subarray using the Quick Sort algorithm
+ * @array: Array to be sorted
+ * @start: Starting index of the array or subarray
+ * @end: Ending index of the array or subarray
+ */
 
 void quick_rec(int *array, int start, int end)
 {
@@ -26,5 +44,13 @@ void quick_rec(int *array, int start, int end)
 	quick_rec(array, pivot + 1, end);
 }
 
+/**
+ * quick_sort - Sorts an array using the Quick Sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+
 void quick_sort(int *array, size_t size)
-{ quick_rec(array, 0, (int)size - 1); }
+{
+	 quick_rec(array, 0, (int)size - 1);
+}
