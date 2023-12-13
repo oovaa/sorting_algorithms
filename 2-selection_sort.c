266e99bf2b10c51203a3ba23c1b9e7b188bbe730
EsraaGD <isra.hatim2@gmail.com>
2023-12-13 06:17:23 -0800

    Done

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ *selection_sort- function that sorts using selection sort algorithm
+ *
+ *@array: array to be sorted
+ *@size: size of the array
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t a, b, c;
+    int tmp;
+
+    if (!array || !size)
+        return;
+    for (a = 0; a < size - 1; ++)
+    {
+        for (b = size - 1, c = a + 1; b > a; b--)
+        {
+            if (array[b] < array[c])
+            {
+                c = b;
+            }
+        }
+        if (array[a] > array[c])
+        {
+            tmp = array[a];
+            array[a] = array[c];
+            array[c] = tmp;
+            print_array(array, size);
+        }
+    }
+}
