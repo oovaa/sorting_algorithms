9432613efa7c75aad09c1964ec386c8063bca9d5
EsraaGD <isra.hatim2@gmail.com>
2023-12-16 07:56:31 -0800

    Bubble fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (a = 0; a < size; a++)
 	{
-		for (b = 0; b < size; b++)
+		for (b = 0; b < size - 1; b++)
 		{
 			if (array[b] > array[b + 1])
 			{
