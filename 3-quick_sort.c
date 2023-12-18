5f90e23b5e30048838806bb8e487ad80af21d5eb
oovaa <omarvx211@gmail.com>
2023-12-18 07:03:28 +0200

    qucik 9 it needs swap_ints at 18-07

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,18 @@
 #include "sort.h"
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 /**
  * partition - Lomuto partition scheme
@@ -23,7 +36,7 @@ int partition(int *array, size_t start, size_t end, size_t size)
 			i++;
 			if (i != j)
 			{
-				swap(&array[i], &array[j]);
+				swap_ints(&array[i], &array[j]);
 				print_array(array, size);
 			}
 		}
@@ -32,7 +45,7 @@ int partition(int *array, size_t start, size_t end, size_t size)
 	i++;
 	if (i != end)
 	{
-		swap(&array[i], &array[end]);
+		swap_ints(&array[i], &array[end]);
 		print_array(array, size);
 	}
 
