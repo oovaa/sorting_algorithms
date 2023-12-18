04d0fb266f31d3306a6a753cd9b4832e91ca6ad8
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-18 13:07:45 +0300

    Starting radix

---
+++ 105-radix_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * radix_sort - sorts an array of integers in ascending order
+ * @array: to be sorted
+ * @size: size of array
+ */
+
+void radix_sort(int *array, size_t size)
+{
+    if (!array || size < 2)
+        return;
+}
